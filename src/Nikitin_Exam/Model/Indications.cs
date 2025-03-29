fc6b8d966dbb5166fd605e18307ad07714fe64de
awaayy <phantomy.design@gmail.com>
2025-03-29 11:28:19 +0300

    Написание программы

---
+++ src/Nikitin_Exam/Model/Indications.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nikitin_Exam.Model
+{
+    public class Indications
+    {
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public double Pressure { get; set; }
+
+        public Indications(double temperature, double humidity, double pressure)
+        {
+            Temperature = temperature;
+            Humidity = humidity;
+            Pressure = pressure;
+        }
+
+        public override string ToString()
+        {
+            return $"Температура: {Temperature}, Влажность: {Humidity}, Давление: {Pressure}";
+        }
+    }
+}
---
+++ src/Nikitin_Exam/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Nikitin_Exam.Model;
+
+namespace Nikitin_Exam
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            WeatherControl weatherControl = new WeatherControl();
+
+            Console.Write("Введите количество показаний: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите температуру (в град. цельсия): ");
+                double temperature = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите влажность (в процентах): ");
+                double humidity = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите давление (в мм. рт. ст.): ");
+                double pressure = double.Parse(Console.ReadLine());
+
+                Indications indication = new Indications(temperature, humidity, pressure);
+                weatherControl.AddIndication(indication);
+            }
+
+            weatherControl.SortIndications();
+            weatherControl.SaveToFile("pogoda.txt");
+        }
+    }
+}
+
+
+
